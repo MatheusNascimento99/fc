7098bcb8e96f30b9d14808f2c35c58029375a56e
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2024-03-11 21:52:47 -0300

    inserção seta para baixo link serviço

--- src/pages/components/a_navbar/index.jsx
+++ src/pages/components/a_navbar/index.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
                         <Link className="Links_Page" to="/about">Sobre Nós</Link>
                     </li>
                     <li id="Dropdown">
-                        <Link className="Links_Page" to="/service">Soluções </Link>
+                        <Link className="Links_Page" to="/service">Soluções <img className="iconSeta" src={Seta} alt="img seta" /> </Link>
                         <div id="Dropdown_Menu">
                             <Link className="Dropdown_Link" to="/telefone">Via Telefone</Link>
                             <Link className="Dropdown_Link" to="/whatsapp">Via WhatsApp</Link>
@@ -38,7 +38,6 @@ const Navbar = () => {
 
 
             <div className="Navbar_Full_SocialMedia">
-
                 <a href="https://www.linkedin.com/in/matheus-nascimento-106a00226/" target="_blank"> <img className="icon" src={Linkedin} alt="img linkedin" /></a>
                 <a href="https://www.linkedin.com/in/matheus-nascimento-106a00226/" target="_blank"> <img className="icon" src={Insta} alt="img instagram" /></a>
                 <a href="https://www.linkedin.com/in/matheus-nascimento-106a00226/" target="_blank"> <img className="icon" src={Whatsapp} alt="img whatsapp" /></a>
